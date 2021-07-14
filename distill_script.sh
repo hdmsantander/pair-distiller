68de822abf9be82aa79e8f323caa682060e36e37
Fabián Santander <hdmsantander@gmail.com>
2021-07-14 14:53:41 -0500

    Exclude tests from analysis

--- distill_script.sh
+++ distill_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 shopt -s globstar
 shopt -s nullglob
+shopt -s extglob
 
 # Print header into results CSV file
 echo "filename, change, change_level, level_name, change_start_byte, change_end_byte, line_number, commit_1, commit_2, change_content" > distill_results.csv
@@ -11,8 +12,8 @@ PREVIOUS_COMMIT=$(git rev-list "$1" --max-count 1 --no-merges --since "$3")
 # Iterate over the commits in order, discarding merge commits, since the date provided. Skip the first commit as we already have it.
 for COMMIT in $(git rev-list "$1" --max-count "$2" --no-merges --since "$3" --skip 1)
 do
-    # Generate changed file list between commit A and commit B
-    git diff --name-only "$PREVIOUS_COMMIT" "$COMMIT" | grep "\.java" > temp
+    # Generate changed file list between commit A and commit B, exclude tests directory
+    git diff --name-only "$PREVIOUS_COMMIT" "$COMMIT" -- ":(exclude)*/test/*" | grep "\.java" > temp
     
     # Read the filenames of the files that changed and copy them to use change distiller
     while read file; do
