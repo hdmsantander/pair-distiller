f4b468808bf7dbd6a18594830c0697fbb4342933
Fabián Santander <hdmsantander@gmail.com>
2021-08-11 15:25:58 -0500

    Update distiller and integrator

--- README.md
+++ README.md
@@ -4,21 +4,41 @@
 
 You can find some results of the project [here](https://hdmsantander.github.io/pair-distiller/)
 
-## pair-distiller
+## Generating CSV (pair-distiller)
 
 To generate the _distill_results.csv file_, run _distill_script.sh_ as follows:
 
 ```BASH
-./distill_script.sh arg1 arg2 arg3
+./distill_script.sh arg1 arg2
 ```
 
-Where `arg1`, `arg2` and `arg3` are:
+Where `arg1` and `arg2` are:
 
-- `arg1`: Name of the branch on which to run git commands, e.g. master
-- `arg2`: Number of commits to work with, e.g. 50
-- `arg3`: Date from which the scan will take place, e.g. 2015-02-18
+- `arg1`: Tag from which to run the tool
+- `arg2`: Number of commits to work with, e.g. 500
 
 The script has to be run at the root of the project to analyze.
+
+## Incorporating CSV information to database (pair-distiller-integrator)
+
+To incorporate the CSV information to the database, run the _pair_distiller_integrator.jar_ as follows:
+
+```BASH
+java -jar pair_distiller_integrator.jar -h DATABASE_HOST -d DATABASE_NAME -u DATABASE_USER -p USER_PASSWORD -c CSV_FILE
+```
+
+This will load the CSV information into the change_details table within the PostgreSQL database using the information from the CSV file.
+
+The program will output errors to STDOUT and print a brief summary of the successes and failures. It will also print an array of line numbers for problematic entries from the CSV file.
+
+Like:
+
+```
+Matched: 241 not matched: 16 ambiguous: 0
+Lines with errors: [30, 87, 89, 92, 94, 97, 98, 103, 183, 185, 186, 187, 190, 192, 193, 194]
+```
+
+## Generating RDA files (r_script)
 
 To generate the RDA files given the _distill_results.csv_ run _r_script.sh_:
 
--- distill_script.sh
+++ distill_script.sh
@@ -4,13 +4,13 @@ shopt -s nullglob
 shopt -s extglob
 
 # Print header into results CSV file
-echo "filename, change, change_level, level_name, change_start_byte, change_end_byte, line_number, commit_1, commit_2, change_content" > distill_results.csv
+echo "filename, name, change, change_level, level_name, change_start_byte, change_end_byte, line_number, commit_1, commit_2, change_content" > distill_results.csv
 
 # Set first commit as base commit
-PREVIOUS_COMMIT=$(git rev-list "$1" --max-count 1 --no-merges --since "$3")
+PREVIOUS_COMMIT=$(git rev-list "$1" --in-commit-order --max-count 1 --no-merges)
 
 # Iterate over the commits in order, discarding merge commits, since the date provided. Skip the first commit as we already have it.
-for COMMIT in $(git rev-list "$1" --max-count "$2" --no-merges --since "$3" --skip 1)
+for COMMIT in $(git rev-list "$1" --in-commit-order --max-count "$2" --no-merges --skip 1)
 do
     # Generate changed file list between commit A and commit B, exclude tests directory
     git diff --name-only "$PREVIOUS_COMMIT" "$COMMIT" -- ":(exclude)*/test/*" | grep "\.java" > temp
@@ -22,19 +22,19 @@ do
         
         # Copy file to a temporary file
         if [ -f "$file" ]; then
-            cp "$file" "$file.A"
+            cp "$file" "file.A"
         fi
         
         git checkout -q "$COMMIT" > /dev/null
         
         # Copy file to a temporary file
         if [ -f "$file" ]; then
-            cp "$file" "$file.B"
+            cp "$file" "file.B"
         fi
         
         # Only if both files exists perform the distilling and append results to CSV file
-        if [[ -f "$file.A" && -f "$file.B" ]]; then
-            java -jar pair_distiller.jar -a "$file.A" -b "$file.B" -c1 "$PREVIOUS_COMMIT" -c2 "$COMMIT" >> distill_results.csv
+        if [[ -f "file.A" && -f "file.B" ]]; then
+            java -jar pair_distiller.jar -f "$file" -a "file.A" -b "file.B" -c1 "$PREVIOUS_COMMIT" -c2 "$COMMIT" >> distill_results.csv
         fi
         
     done < temp
@@ -42,5 +42,21 @@ do
     PREVIOUS_COMMIT=$COMMIT
     
 done
-exit 0
 
+# Cleanup temporary file pairs
+if [[ -f "file.A" && -f "file.B" ]]; then
+    rm "file.A"
+    rm "file.B"
+fi
+
+# Cleanup temp list of files
+if [ -f "temp" ]; then
+    rm "temp"
+fi
+
+# Cleanup pid files if there are any
+if [ "$(ls -la | grep -c .attach_pid)" -gt 0 ]; then
+    rm .attach_pid*
+fi
+
+exit 0
